7cac1d3df1adbceb042848791d0dc523356408c5
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-07 18:42:58 -0400

    finished test for employee test

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -2,15 +2,48 @@ const Employee = require("../lib/employee");
 
 describe("Employee", () => {
     it("Creates employee object", () => {
-        const newEmployee = new Employee();
-        expect(typeof(newEmployee)).toBe("object");
+        const e = new Employee();
+        expect(typeof(e)).toBe("object");
     });
 
-    it("Creates employee object with name, id and email properties", () => {
-        const newEmployee = new Employee("Josh", "0", "test@test.com");
+    it("Creates employee object and sets name, id and email properties", () => {
+        const e = new Employee("Josh", "0", "test@test.com");
 
-        expect(newEmployee.name).toEqual("Josh");
-        expect(newEmployee.id).toEqual("0");
-        expect(newEmployee.email).toEqual("test@test.com");
-    })
+        expect(e.name).toEqual("Josh");
+        expect(e.id).toEqual("0");
+        expect(e.email).toEqual("test@test.com");
+    });
+
+    describe("getName", () => {
+        it("Gets name", () => {
+            const testValue = "Josh"
+            const e = new Employee(testValue);
+            expect(e.getName()).toBe(testValue);
+        });
+    });
+
+    describe("getId", () => {
+        it("Gets id", () => {
+            const testValue = "0"
+            const e = new Employee("Josh", testValue);
+            expect(e.getId()).toBe(testValue);
+        });
+    });
+
+    describe("getEmail", () => {
+        it("Gets email", () => {
+            const testValue = "test@test.com"
+            const e = new Employee("Josh", "0", testValue);
+            expect(e.getEmail()).toBe(testValue);
+        });
+    });
+
+    describe("getRole", () => {
+        it("Gets name", () => {
+            const testValue = "Employee"
+            const e = new Employee("Josh", "0", "test@test.com");
+            expect(e.getRole()).toBe(testValue);
+        });
+    });
+    
 });
