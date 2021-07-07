c58980308b31af2ed017db4b99d864d79399ffe0
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-07 18:16:45 -0400

    created test for employee object and properties

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,1 +1,16 @@
 const Employee = require("../lib/employee");
+
+describe("Employee", () => {
+    it("Creates employee object", () => {
+        const newEmployee = new Employee();
+        expect(typeof(newEmployee)).toBe("object");
+    });
+
+    it("Creates employee object with name, id and email properties", () => {
+        const newEmployee = new Employee("Josh", "0", "test@test.com");
+
+        expect(newEmployee.name).toEqual("Josh");
+        expect(newEmployee.id).toEqual("0");
+        expect(newEmployee.email).toEqual("test@test.com");
+    })
+});
