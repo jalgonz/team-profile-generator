4f29ef67f855efebbf2f1610c1a5f5268c2db12c
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-07 21:31:23 -0400

    completed engineer test

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,1 +1,35 @@
+const Employee = require("../lib/employee");
 const Engineer = require("../lib/engineer");
+
+describe("Engineer", () => {
+    it("Creates engineer object", () => {
+        const e = new Engineer();
+        expect(typeof(e)).toBe("object");
+    });
+
+    it("Sets name, id, email, and gitHub properties", () => {
+        const e = new Employee("Josh", "0", "test@test.com", "jalgonz");
+
+        expect(e.name).toEqual("Josh");
+        expect(e.id).toEqual("0");
+        expect(e.email).toEqual("test@test.com");
+        expect(e.gitHub).toEqual("jalgonz")
+    });
+
+    describe("getGitHub", () => {
+        it("Gets GitHub", () => {
+            const testValue = "jalgonz";
+            const e = new Employee("Josh", "0", "test@test.com", testValue);
+            expect(e.getGitHub()).toBe(testValue);
+        });
+    });
+
+    describe("getRole", () =>{
+        it("Gets role", () => {
+            const testValue = "Engineer";
+            const e = new Employee("Josh", "0", "test@test.com", "jalgonz");
+            expect(e.getRole()).toBe(testValue);
+        });
+    });
+
+});
