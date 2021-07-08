6e9369ea6cda568c025f87b251ec687e4521fea1
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-07 22:01:45 -0400

    cleaned code for engineer test

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,19 +1,9 @@
-const Employee = require("../lib/employee");
 const Engineer = require("../lib/engineer");
 
 describe("Engineer", () => {
-    it("Creates engineer object", () => {
-        const e = new Engineer();
-        expect(typeof(e)).toBe("object");
-    });
-
     it("Sets name, id, email, and gitHub properties", () => {
         const e = new Engineer("Josh", "0", "test@test.com", "jalgonz");
-
-        expect(e.name).toEqual("Josh");
-        expect(e.id).toEqual("0");
-        expect(e.email).toEqual("test@test.com");
-        expect(e.gitHub).toEqual("jalgonz")
+        expect(e.gitHub).toEqual("jalgonz");
     });
 
     describe("getGitHub", () => {
