ac660e9e7fadb18ebb7bcce766ae20292b86c253
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-07 22:22:52 -0400

    finished and oassed manager test

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,1 +1,25 @@
-const manager = require("../lib/manager");
+const Manager = require("../lib/manager");
+
+describe("Manager", () => {
+    it("Sets name, id, email, and officeNumber properties", () => {
+        const e = new Manager("Josh", "0", "test@test.com", "1234");
+        expect(e.officeNumber).toEqual("1234");
+    });
+
+    describe("getOfficeNumber", () => {
+        it("Gets office number", () => {
+            const testValue = "1234";
+            const e = new Manager("Josh", "0", "test@test.com", testValue);
+            expect(e.getOfficeNumber()).toBe(testValue);
+        });
+    });
+
+    describe("getRole", () => {
+        it("Gets role", () => {
+            const testValue = "Manager";
+            const e = new Manager("Josh", "0", "test@test.com", "1234");
+            expect(e.getRole()).toBe(testValue);
+        });
+    });
+    
+});
